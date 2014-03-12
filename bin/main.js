e262e5a33b6f4b0802cd544135701f08f24d1c74
p4ul <p4ul.sincl4ir@gmail.com>
2014-03-12 22:45:51 +1300

    * added chrome API messaging * editing / resetting a username will now reload the plugin properly * made the "stopped coding" notification off by default * now if you untick a notification and click authenticate it will no longer save refs BeryJu/Codeivate-Chrome#5 * bumped version * misc code formating

--- bin/main.js
+++ bin/main.js
@@ -20,4 +20,40 @@ chrome.runtime.onInstalled.addListener(function() {
 		chrome.tabs.create({ url: "options/index.html" });
 	}
 });
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log(sender.tab ?
+            "from a content script:" + sender.tab.url :
+            "from the extension");
+        if (request.action == "restart") {
+            callb();
+            sendResponse({farewell: "goodbye"});
+        }
+});
+
+//chrome.runtime.onUpdateAvailable.addListener(function(details) {
+//    console.log("updating to version " + details.version);
+//    chrome.runtime.reload();
+//});
+//
+//chrome.runtime.requestUpdateCheck(function(status) {
+//    if (status == "update_available") {
+//        console.log("update pending...");
+//    } else if (status == "no_update") {
+//        console.log("no update found");
+//    } else if (status == "throttled") {
+//        console.log("Oops, I'm asking too frequently - I need to back off.");
+//    }
+//});
+//
+//chrome.runtime.onMessage.addListener(
+//    function(request, sender, sendResponse) {
+//        console.log(sender.tab ?
+//            "from a content script:" + sender.tab.url :
+//            "from the extension");
+//        if (request.greeting == "hello")
+//            sendResponse({farewell: "goodbye"});
+//    });
 callb();
+
--- bin/options/options.js
+++ bin/options/options.js
@@ -1,75 +1,92 @@
-$(document).ready(function(){
+/*globals chrome*/
+$(document).ready(function () {
 
-	if (localStorage['user'] !== undefined) {
-		// $("#submit").prop('disabled', true);
-		$("#username").val(localStorage['user']);
-		// $("#username").prop('disabled',true);
-		$("#submit").html("Authenticated!");
+    function pageLoad() {
+        $('#notificationStoppedCoding').prop('checked', true);
+        $('#notificationLangLevel').prop('checked', localStorage['notificationLangLevel'] === "true");
+        $('#notificationStoppedCoding').prop('checked', localStorage['notificationStoppedCoding'] === "true");
+        $('#notificationHourGained').prop('checked', localStorage['notificationHourGained'] === "true");
+        if (localStorage['user'] !== undefined) {
+            // $("#submit").prop('disabled', true);
+            $("#username").val(localStorage['user']);
+            // $("#username").prop('disabled',true);
+            $("#submit").html("Authenticated!");
 
-		$('#links').append($('<li>').html(
-			$("<a>")
-			.attr('href','http://codeivate.com/users/' + localStorage['user'])
-			.attr('target', '_blank')
-			.html(localStorage['user']+" on Codeivate"))
-		);
-	}
+            $('#links').append($('<li>').html(
+                $("<a>")
+                    .attr('href', 'http://codeivate.com/users/' + localStorage['user'])
+                    .attr('target', '_blank')
+                    .html(localStorage['user'] + " on Codeivate"))
+            );
+        }
+    }
 
-	$('#reset').click(function(){
-		console.log('resetting');
-		localStorage.clear();
-
-		$('#submit').prop('disabled',false).removeClass('disabled').button('reset');
-		$('#username').prop('disabled', false);
-		$('#submit').removeClass('btn-primary btn-success btn-warning');
-		 $('.form-group').removeClass('has-success');
-		$('#submit').addClass('btn-primary');
-	});
-
-	$('#submit').click(function(){
-
-		var username = $("#username").val();
+    function reloadPlugin() {
+        chrome.runtime.sendMessage({action: "restart"}, function (response) {
+            console.log(response.farewell);
+        });
+    }
 
 
-		$('#submit').button('loading');
-		if (username !== "") {
-			var codeivate = new Codeivate.Extension(username, document);
-			codeivate.request(function(res, stauts){
-				var raw = JSON.parse(res);
-				var data = new Codeivate.User(raw);
-				if ( typeof data.error == "undefined") {
-					localStorage['user'] = data.name;
+    $('#reset').click(function () {
+        console.log('resetting');
+        localStorage.clear();
 
-					localStorage['notificationLangLevel'] = $('#notificationLangLevel').prop('checked');
-					localStorage['notificationStoppedCoding'] = $('#notificationStoppedCoding').prop('checked');
-					localStorage['notificationHourGained'] = $('#notificationHourGained').prop('checked');
+        $('#submit').prop('disabled', false).removeClass('disabled').button('reset');
+        $('#username').prop('disabled', false);
+        $('#submit').removeClass('btn-primary btn-success btn-warning');
+        $('.form-group').removeClass('has-success');
+        $('#submit').addClass('btn-primary');
+        reloadPlugin();
 
-					$("submit").prop('disabled', true);
-					$("username").prop('disabled', true);
-					$('#submit').removeClass('btn-primary btn-success btn-warning');
-					$('#submit').addClass('btn-success');
+    });
 
-					$('#submit').button('reset');
-					$('.form-group').removeClass('has-error');
-					$('.form-group').addClass('has-success');
-					$('.username_error').empty();
-					//don't know if we need this?
-					//chrome.runtime.reload()
-				} else {
-					$('#submit').removeClass('btn-primary btn-success btn-warning');
-					$('#submit').addClass('btn-primary');
-					$('.form-group').addClass('has-error');
-					$('.username_error').html($('<strong>').html('enter a valid username'));
-					$('#submit').button('reset');
-					console.error("status code: " + response.status);
-				}
-			});
-		} else {
-			alert('please enter a username');
-			$('#submit').removeClass('btn-primary btn-success btn-warning');
-			$('#submit').addClass('btn-warning');
-			$('#submit').button('reset');
+    $('#submit').click(function () {
 
-		}
-	});
+        var username = $("#username").val();
+
+
+        $('#submit').button('loading');
+        if (username !== "") {
+            var codeivate = new Codeivate.Extension(username, document);
+            codeivate.request(function (res, stauts) {
+                var raw = JSON.parse(res);
+                var data = new Codeivate.User(raw);
+                if (typeof data.error == "undefined") {
+                    localStorage['user'] = data.name;
+
+                    localStorage['notificationLangLevel'] = $('#notificationLangLevel').prop('checked');
+                    localStorage['notificationStoppedCoding'] = $('#notificationStoppedCoding').prop('checked');
+                    localStorage['notificationHourGained'] = $('#notificationHourGained').prop('checked');
+
+                    $("submit").prop('disabled', true);
+                    $("username").prop('disabled', true);
+                    $('#submit').removeClass('btn-primary btn-success btn-warning');
+                    $('#submit').addClass('btn-success');
+
+                    $('#submit').button('reset');
+                    $('.form-group').removeClass('has-error');
+                    $('.form-group').addClass('has-success');
+                    $('.username_error').empty();
+                    reloadPlugin();
+                } else {
+                    $('#submit').removeClass('btn-primary btn-success btn-warning');
+                    $('#submit').addClass('btn-primary');
+                    $('.form-group').addClass('has-error');
+                    $('.username_error').html($('<strong>').html('enter a valid username'));
+                    $('#submit').button('reset');
+                    console.error("status code: " + response.status);
+                }
+            });
+        } else {
+            alert('please enter a username');
+            $('#submit').removeClass('btn-primary btn-success btn-warning');
+            $('#submit').addClass('btn-warning');
+            $('#submit').button('reset');
+
+        }
+    });
+
+    pageLoad();
 
 });
